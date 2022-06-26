cfb8f46cc4d2817886ace8fa1ae1e2331f9fb0ce
FlashHit <56718716+FlashHit@users.noreply.github.com>
2022-06-26 17:22:20 +0200

    refactor(ServerBannersAndLevelNames): improve code

--- ext/Server/ServerBannersAndLevelNames.lua
+++ ext/Server/ServerBannersAndLevelNames.lua
@@ -1,240 +1,113 @@
 Events:Subscribe('Level:LoadResources', function()
-
-    -- Most of these are AI upscaled from 480p or something, and so look questionable. 
-    -- Only Thunder Run has a custom image. 
-    -- Feel free to take your own and replace the image. If you're willing, I'd appreciate it hugely if you could send your custom image to me to put it into the mod.
-
-    -- Hit and Run COOP_002 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/COOP_002/COOP_002' then
-
-        -- Set custom map name
-        ServerUtils:SetCustomMapName('Hit and Run')
-
-        -- Set server banner
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/zv33mTZ.jpg'})
-
-        goto cont
-
-    end
-
-    -- Drop 'Em Like Liquid COOP_003 ------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/COOP_003/COOP_003' then
-
-        ServerUtils:SetCustomMapName('Drop \'Em Like Liquid')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/aev015H.jpg'})
-
-        goto cont
-
-    end
-
-    -- Fire from the Sky COOP_006 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/COOP_006/COOP_006' then
-
-        ServerUtils:SetCustomMapName('Aygan Village')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/4x5sdl4.jpg'})
-
-        goto cont
-
-    end
-
-    -- Operation Exodus COOP_007 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/COOP_007/COOP_007' then
-
-        ServerUtils:SetCustomMapName('Operation Exodus')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/OHTswJF.jpg'})
-
-        goto cont
-
-    end
-
-    -- Exfiltration COOP_009 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/COOP_009/COOP_009' then
-
-        ServerUtils:SetCustomMapName('Exfiltration')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/Tf5j63i.jpg'})
-
-        goto cont
-
-    end
-
-    -- The Eleventh Hour COOP_010 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/COOP_010/COOP_010' then
-
-        ServerUtils:SetCustomMapName('The Eleventh Hour')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/9E8IBc3.jpg'})
-
-        goto cont
-
-    end
-
-    -- Operation Guillotine SP_Bank -------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Bank/SP_Bank' then
-
-        ServerUtils:SetCustomMapName('Operation Guillotine')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/4hvwq7U.jpg'})
-
-        goto cont
-
-    end
-
-    -- Operation Swordbreaker SP_Earthquake -----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Earthquake/SP_Earthquake' then
-
-        ServerUtils:SetCustomMapName('Operation Swordbreaker')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/i1GW3vx.jpg'})
-
-        goto cont
-
-    end
-
-    -- Uprising SP_Earthquake2 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Earthquake2/SP_Earthquake2' then
-
-        ServerUtils:SetCustomMapName('Uprising')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/RYPsLuZ.jpg'})
-
-        goto cont
-
-    end
-
-    -- The Great Destroyer SP_Finale ------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Finale/SP_Finale' then
-
-        ServerUtils:SetCustomMapName('The Great Destroyer')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/8nrcHLT.jpg'})
-
-        goto cont
-
-    end
-
-    -- Going Hunting SP_Jet ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Jet/SP_Jet' then
-
-        ServerUtils:SetCustomMapName('Carrier')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/GzPgiX9.jpg'})
-
-        goto cont
-
-    end
-
-    -- Semper Fidelis SP_New_York ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_New_York/SP_New_York' then
-
-        ServerUtils:SetCustomMapName('Semper Fidelis')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/qz9xETm.jpg'})
-
-        goto cont
-
-    end
-
-    -- Comrades SP_Paris ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Paris/SP_Paris' then
-
-        ServerUtils:SetCustomMapName('Comrades')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/mTlnWg9.jpg'})
-
-        goto cont
-
-    end
-
-    -- Night Shift SP_Sniper --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Sniper/SP_Sniper' then
-
-        ServerUtils:SetCustomMapName('Night Shift')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/WSjk0So.jpg'})
-
-        goto cont
-
-    end
-
-    -- Thunder Run SP_Tank ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Tank/SP_Tank' then
-
-        ServerUtils:SetCustomMapName('Thunder Run')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/eiDLOtJ.jpg'})
-
-        goto cont
-
-    end
-
-    -- Fear No Evil SP_Tank_B -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Tank_b/SP_Tank_b' then
-
-        ServerUtils:SetCustomMapName('Fear No Evil')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/mV3guKq.jpg'})
-
-        goto cont
-
-    end
-
-    -- Rock and a Hard Place SP_Valley ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Valley/SP_Valley' then
-
-        ServerUtils:SetCustomMapName('Valley')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/GPZAhT0.jpg'})
-
-        goto cont
-
-    end
-
-    -- Kaffarov's Villa SP_Villa ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/SP_Villa/SP_Villa' then
-
-       ServerUtils:SetCustomMapName('Kaffarov\'s Villa')
-        RCON:SendCommand('vu.ServerBanner', {'https://i.imgur.com/VS4sq6j.jpg'})
-
-        goto cont
-
-    end
-	
-	-- Metro Extended ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    if SharedUtils:GetLevelName() == 'Levels/MP_Subway/MP_Subway' and SharedUtils:GetCurrentGameMode() == 'ConquestAssaultLarge0' then
-
-        ServerUtils:SetCustomMapName('Metro Extended')
-		RCON:SendCommand('vu.ServerBanner', {''})
-
-        goto cont
-
-    end
-
-    RCON:SendCommand('vu.ServerBanner', {''})
-	ServerUtils:ClearCustomMapName()
-
-    ::cont::
-
-end)
-
-
-
-
---King of the Hill
-
-Events:Subscribe('Level:LoadResources', function()
-	if SharedUtils:GetLevelName() == "Levels/COOP_006/COOP_006" and SharedUtils:GetCurrentGameMode() == "TankSuperiority0" then
-		if ServerUtils:GetCustomGameModeName() == nil or ServerUtils:GetCustomGameModeName() ~= 'King of The Hill' then
+	local s_LevelName = SharedUtils:GetLevelName()
+
+	-- Most of these are AI upscaled from 480p or something, and so look questionable.
+	-- Only Thunder Run has a custom image.
+	-- Feel free to take your own and replace the image. If you're willing, I'd appreciate it hugely if you could send your custom image to me to put it into the mod.
+
+	-- Hit and Run COOP_002
+	if s_LevelName == 'Levels/COOP_002/COOP_002' then
+		-- Set custom map name
+		ServerUtils:SetCustomMapName('Hit and Run')
+		-- Set server banner
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/zv33mTZ.jpg' })
+
+	-- Drop 'Em Like Liquid COOP_003
+	elseif s_LevelName == 'Levels/COOP_003/COOP_003' then
+		ServerUtils:SetCustomMapName('Drop \'Em Like Liquid')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/aev015H.jpg' })
+
+	-- Fire from the Sky COOP_006
+	elseif s_LevelName == 'Levels/COOP_006/COOP_006' then
+		if SharedUtils:GetCurrentGameMode() == "TankSuperiority0" then
 			ServerUtils:SetCustomGameModeName('King of The Hill')
+		else
+			ServerUtils:SetCustomMapName('Aygan Village')
+			RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/4x5sdl4.jpg' })
 		end
-	elseif ServerUtils:GetCustomGameModeName() ~= nil and ServerUtils:GetCustomGameModeName() == 'King of The Hill' then
-		ServerUtils:ClearCustomGameModeName()
+
+	-- Operation Exodus COOP_007
+	elseif s_LevelName == 'Levels/COOP_007/COOP_007' then
+		ServerUtils:SetCustomMapName('Operation Exodus')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/OHTswJF.jpg' })
+
+	-- Exfiltration COOP_009
+	elseif s_LevelName == 'Levels/COOP_009/COOP_009' then
+		ServerUtils:SetCustomMapName('Exfiltration')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/Tf5j63i.jpg' })
+
+	-- The Eleventh Hour COOP_010
+	elseif s_LevelName == 'Levels/COOP_010/COOP_010' then
+		ServerUtils:SetCustomMapName('The Eleventh Hour')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/9E8IBc3.jpg' })
+
+	-- Operation Guillotine SP_Bank
+	elseif s_LevelName == 'Levels/SP_Bank/SP_Bank' then
+		ServerUtils:SetCustomMapName('Operation Guillotine')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/4hvwq7U.jpg' })
+
+	-- Operation Swordbreaker SP_Earthquake
+	elseif s_LevelName == 'Levels/SP_Earthquake/SP_Earthquake' then
+		ServerUtils:SetCustomMapName('Operation Swordbreaker')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/i1GW3vx.jpg' })
+
+	-- Uprising SP_Earthquake2
+	elseif s_LevelName == 'Levels/SP_Earthquake2/SP_Earthquake2' then
+		ServerUtils:SetCustomMapName('Uprising')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/RYPsLuZ.jpg' })
+
+	-- The Great Destroyer SP_Finale
+	elseif s_LevelName == 'Levels/SP_Finale/SP_Finale' then
+		ServerUtils:SetCustomMapName('The Great Destroyer')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/8nrcHLT.jpg' })
+
+	-- Going Hunting SP_Jet
+	elseif s_LevelName == 'Levels/SP_Jet/SP_Jet' then
+		ServerUtils:SetCustomMapName('Carrier')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/GzPgiX9.jpg' })
+
+	-- Semper Fidelis SP_New_York
+	elseif s_LevelName == 'Levels/SP_New_York/SP_New_York' then
+		ServerUtils:SetCustomMapName('Semper Fidelis')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/qz9xETm.jpg' })
+
+	-- Comrades SP_Paris
+	elseif s_LevelName == 'Levels/SP_Paris/SP_Paris' then
+		ServerUtils:SetCustomMapName('Comrades')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/mTlnWg9.jpg' })
+
+	-- Night Shift SP_Sniper
+	elseif s_LevelName == 'Levels/SP_Sniper/SP_Sniper' then
+		ServerUtils:SetCustomMapName('Night Shift')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/WSjk0So.jpg' })
+
+	-- Thunder Run SP_Tank
+	elseif s_LevelName == 'Levels/SP_Tank/SP_Tank' then
+		ServerUtils:SetCustomMapName('Thunder Run')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/eiDLOtJ.jpg' })
+
+	-- Fear No Evil SP_Tank_B
+	elseif s_LevelName == 'Levels/SP_Tank_b/SP_Tank_b' then
+		ServerUtils:SetCustomMapName('Fear No Evil')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/mV3guKq.jpg' })
+
+	-- Rock and a Hard Place SP_Valley
+	elseif s_LevelName == 'Levels/SP_Valley/SP_Valley' then
+		ServerUtils:SetCustomMapName('Valley')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/GPZAhT0.jpg' })
+
+	-- Kaffarov's Villa SP_Villa
+	elseif s_LevelName == 'Levels/SP_Villa/SP_Villa' then
+		ServerUtils:SetCustomMapName('Kaffarov\'s Villa')
+		RCON:SendCommand('vu.ServerBanner', { 'https://i.imgur.com/VS4sq6j.jpg' })
+
+	-- Metro Extended
+	elseif s_LevelName == 'Levels/MP_Subway/MP_Subway' and SharedUtils:GetCurrentGameMode() == 'ConquestAssaultLarge0' then
+		ServerUtils:SetCustomMapName('Metro Extended')
+		RCON:SendCommand('vu.ServerBanner', { '' })
+
+	else
+		ServerUtils:ClearCustomMapName()
+		RCON:SendCommand('vu.ServerBanner', { '' })
 	end
 end)
